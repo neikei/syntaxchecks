8c555fdadb36a8d6ccd274ddbeb72cb917e8a5e8
neikei <niklas@vosskoetter.org>
2018-07-01 15:44:33 +0200

    Improvements with recommendations from shellchecker

--- syntaxchecks.sh
+++ syntaxchecks.sh
@@ -13,7 +13,7 @@ regex_crontab_files="^.*\.cron$"
 # exclude vendor and cache directories
 regex_exclude="^.*/(vendor|cache)/.*$"
 # script path
-script_path="`dirname \"$0\"`"
+script_path=$(dirname "$0")
 
 ### functions
 function usage() {
@@ -21,34 +21,34 @@ function usage() {
 }
 
 function system_check() {
-    if [[ ! `which php` ]]; then echo "PHP is not installed."; exit 1; fi
-    if [[ ! `which python` ]]; then echo "Python is not installed."; exit 1; fi
-    if [[ ! `which ruby` ]]; then echo "Ruby is not installed."; exit 1; fi
+    if [[ ! $(which php) ]]; then echo "PHP is not installed."; exit 1; fi
+    if [[ ! $(which python) ]]; then echo "Python is not installed."; exit 1; fi
+    if [[ ! $(which ruby) ]]; then echo "Ruby is not installed."; exit 1; fi
     echo "ok"
 }
 
 function get_file_list() {
     if [ $last_commits -eq 0 ]; then
-        filelist=`find $path -type f -regextype posix-extended -regex "$1" | grep -v -E "$regex_exclude"`
+        filelist=$(find $path -type f -regextype posix-extended -regex "$1" | grep -v -E "$regex_exclude")
     else
-        filelist=`cd $path && git diff --name-only --diff-filter=ACMR HEAD~$last_commits..HEAD | grep -E "$1" | grep -v -E "$regex_exclude"`
+        filelist=$(cd $path && git diff --name-only --diff-filter=ACMR HEAD~$last_commits..HEAD | grep -E "$1" | grep -v -E "$regex_exclude")
     fi
 }
 
 function check_bash_files() {
     echo "==> Check Bash files"
 
-    get_file_list $regex_bash_files
+    get_file_list "$regex_bash_files"
 
     for file in $filelist
     do
-    bash -n $file > /dev/null 2>&1
+    bash -n "$file" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "No syntax errors detected in $file"
     else
         echo "Some syntax errors detected in $file"
         if [ $strict -eq 1 ]; then
-            bash -n $file
+            bash -n "$file"
             exit 1;
         fi
     fi
@@ -60,17 +60,17 @@ function check_bash_files() {
 function check_php_files() {
     echo "==> Check PHP files"
 
-    get_file_list $regex_php_files
+    get_file_list "$regex_php_files"
 
     for file in $filelist
     do
-    php -l $file > /dev/null 2>&1
+    php -l "$file" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "No syntax errors detected in $file"
     else
         echo "Some syntax errors detected in $file"
         if [ $strict -eq 1 ]; then
-            php -l $file
+            php -l "$file"
             exit 1;
         fi
     fi
@@ -82,17 +82,17 @@ function check_php_files() {
 function check_python_files() {
     echo "==> Check Python files"
 
-    get_file_list $regex_python_files
+    get_file_list "$regex_python_files"
 
     for file in $filelist
     do
-    python -m py_compile $file > /dev/null 2>&1
+    python -m py_compile "$file" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "No syntax errors detected in $file"
     else
         echo "Some syntax errors detected in $file"
         if [ $strict -eq 1 ]; then
-            python -m py_compile $file
+            python -m py_compile "$file"
             exit 1;
         fi
     fi
@@ -104,17 +104,17 @@ function check_python_files() {
 function check_ruby_files() {
     echo "==> Check Ruby files"
 
-    get_file_list $regex_ruby_files
+    get_file_list "$regex_ruby_files"
 
     for file in $filelist
     do
-    ruby -c $file > /dev/null 2>&1
+    ruby -c "$file" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "No syntax errors detected in $file"
     else
         echo "Some syntax errors detected in $file"
         if [ $strict -eq 1 ]; then
-            ruby -c $file
+            ruby -c "$file"
             exit 1;
         fi
     fi
@@ -126,7 +126,7 @@ function check_ruby_files() {
 function check_yaml_files() {
     echo "==> Check YAML files"
 
-    get_file_list $regex_yaml_files
+    get_file_list "$regex_yaml_files"
 
     for file in $filelist
     do
@@ -148,17 +148,17 @@ function check_yaml_files() {
 function check_crontab_files() {
     echo "==> Check Crontab files"
 
-    get_file_list $regex_crontab_files
+    get_file_list "$regex_crontab_files"
 
     for file in $filelist
     do
-    $script_path/bin/chkcrontab/chkcrontab $file > /dev/null 2>&1
+    "$script_path"/bin/chkcrontab/chkcrontab "$file" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "No syntax errors detected in $file"
     else
         echo "Some syntax errors detected in $file"
         if [ $strict -eq 1 ]; then
-            $script_path/bin/chkcrontab/chkcrontab $file
+            "$script_path"/bin/chkcrontab/chkcrontab "$file"
             exit 1;
         fi
     fi
